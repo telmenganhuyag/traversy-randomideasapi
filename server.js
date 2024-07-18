9c71fb6d5ff79e1475fa1429a1269916d8d43402
Telmen <telmenganhuyag@gmail.com>
2024-07-19 04:04:01 +0800

    final

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const port = 5000;
-const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
 
+connectDB();
+
+const app = express();
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
