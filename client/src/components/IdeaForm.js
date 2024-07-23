3cb45255aa25fc0077c6564ce77ef34fe7517fdf
Telmen <telmenganhuyag@gmail.com>
2024-07-23 16:51:25 +0800

    final push

--- client/src/components/IdeaForm.js
+++ client/src/components/IdeaForm.js
@@ -1,6 +1,10 @@
+import IdeasApi from "../services/IdeasApi";
+import IdeaList from "./IdeaList";
+
 class IdeaForm { 
     constructor() { 
         this._formModal = document.querySelector('#form-modal');
+        this._ideaList = new IdeaList();
         
     }
 
@@ -8,8 +12,17 @@ class IdeaForm {
         this._form.addEventListener('submit', this.handleSubmit.bind(this))
     }
 
-    handleSubmit(e) { 
+    async handleSubmit(e) { 
         e.preventDefault();
+
+      if (!this._form.elements.text.value || !this._form.elements.tag.value
+         || !this._form.elements.username.value) { 
+          alert('Please enter all fields');
+          return;
+         }
+
+        //  Save user to local storage
+        localStorage.setItem('username', this._form.elements.username.value)
 
         const idea = { 
             text: this._form.elements.text.value,
@@ -17,13 +30,19 @@ class IdeaForm {
             username: this._form.elements.username.value
         };
 
-        console.log(idea);
+        // Add idea to server
+        const newIdea = await IdeasApi.createIdea(idea);
+
+        // Add idea to list
+        this._ideaList.addIdeaToList(newIdea.data.data);
 
         // Clear Fields
 
         this._form.elements.text.value = '';
         this._form.elements.tag.value = '';
         this._form.elements.username.value = '';
+
+        this.render();
 
         document.dispatchEvent(new Event('closemodal'));
     }
@@ -32,7 +51,10 @@ class IdeaForm {
         this._formModal.innerHTML = `<form id="idea-form">
           <div class="form-control">
             <label for="idea-text">Enter a Username</label>
-            <input type="text" name="username" id="username" />
+            <input type="text" name="username" id="username"
+            value="${localStorage.getItem('username') ? localStorage.
+              getItem('username') : ''
+            }" />
           </div>
           <div class="form-control">
             <label for="idea-text">What's Your Idea?</label>
--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -1,22 +1,11 @@
+import IdeasApi from "../services/IdeasApi";
+
 class IdeaList { 
     constructor() { 
         this._ideaListEl = document.querySelector('#idea-list');
-        this._ideas = [
-            {
-                id: 1,
-                text: 'Idea 1',
-                tag: 'Business',
-                username: 'John',
-                date: '02/01/2023'
-            },
-            {
-                id: 2,
-                text: 'Idea 2',
-                tag: 'Technology',
-                username: 'Boom',
-                date: '02/01/2024'
-            },
-        ];
+        this._ideas = [];
+        this.getIdeas();
+
         this._validTags = new Set();
         this._validTags.add('technology');
         this._validTags.add('software');
@@ -24,6 +13,42 @@ class IdeaList {
         this._validTags.add('education');
         this._validTags.add('health');
         this._validTags.add('inventions');
+    }
+
+    addEventListeners() { 
+        this._ideaListEl.addEventListener('click', (e) => { 
+            if (e.target.classList.contains('fa-times')) { 
+                e.stopImmediatePropagation();
+                const ideaId = e.target.parentElement.parentElement.dataset.id;
+                this.deleteIdea(ideaId);
+            }
+        })
+    }
+
+    async getIdeas() { 
+        try {
+            const res = await IdeasApi.getIdeas();
+            this._ideas = res.data.data;
+            this.render();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async deleteIdea(ideaId) { 
+        try {
+            // Delete from server
+            const res = await IdeasApi.deleteIdea(ideaId);
+            this._ideas.filter((idea) => idea._id !== ideaId);
+            this.getIdeas();
+        } catch (error) {
+            alert('You cannot delete this resource');
+        }
+    }
+
+    addIdeaToList(idea) { 
+        this._ideas.push(idea);
+        this.render();
     }
 
     getTagClass(tag) { 
@@ -40,8 +65,10 @@ class IdeaList {
     render() { 
         this._ideaListEl.innerHTML = this._ideas.map((idea) => { 
             const tagClass = this.getTagClass(idea.tag);
-            return ` <div class="card">
-          <button class="delete"><i class="fas fa-times"></i></button>
+            const deleteBtn = idea.username === localStorage.
+            getItem('username') ? `<button class="delete"><i class="fas fa-times"></i></button>` : '';
+            return ` <div class="card" data-id="${idea._id}">
+          ${deleteBtn}
           <h3>
             ${idea.text}
           </h3>
@@ -53,6 +80,7 @@ class IdeaList {
         </div>
             `;
         }).join('');
+        this.addEventListeners();
     }
 }
 
